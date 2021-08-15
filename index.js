89b42a6ab2e247dc6e1e8b7419598cdbcd38f23a
Javier Munster <javier.munster@gmail.com>
2021-08-15 15:39:17 -0400

    new: added SES email functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const AWS = require('aws-sdk');
 const ses = new AWS.SESV2({ apiVersion: '2019-09-27' });
 
+const ToAddress = !!process.env.TO_ADDRESS ? process.env.TO_ADDRESS.split(',') : ["contact@coromite.com"];
+const CcAddress = !!process.env.CC_ADDRESS ? process.env.CC_ADDRESS.split(',') : [];
 
 function isValidBody({ firstName, lastName, email, message }) {
     const isValidString = (data, isRequired = false) => {
@@ -14,16 +16,52 @@ function isValidBody({ firstName, lastName, email, message }) {
 
 exports.handler = async(event) => {
     try {
-        const body = JSON.parse(event.body);
+        const { firstName, lastName, email, message } = JSON.parse(event.body);
 
-        console.log("Invoked:", body);
+        console.log("Invoked:", { firstName, lastName, email, message });
 
-        if (!isValidBody(body)) {
+        if (!isValidBody({ firstName, lastName, email, message })) {
             console.warn("Invalid body!");
             return Promise.resolve({
                 statusCode: 422
             });
         }
+
+        return new Promise((resolve) => {
+            return ses.sendEmail({
+                Destination: {
+                    ToAddress,
+                    CcAddress
+                },
+                Message: {
+                    Subject: {
+                        Charset: "UTF-8",
+                        Data: "Coromite - New Contact"
+                    },
+                    Text: {
+                        Charset: "UTF-8",
+                        Data: `First Name: ${firstName}\nLast Name: ${lastName}\nemail: ${email}\n\nMessage:\n${message}`
+                    },
+                    // Html: {
+                    //     Charset: "UTF-8",
+                    //     Data: `<h1>Coromite - New Contact</h1><p>First Name: ${firstName}</p><p>Last Name: ${lastName}</p><p>email: ${email}</p><p>Message: ${message}</p>`
+                    // }
+                },
+                Source: "noreply@coromite.com",
+            }, (err, data) => {
+                if (err) {
+                    console.error("Error sending email:", err);
+                    return resolve({
+                        statusCode: 500
+                    });
+                }
+
+                console.log("Email sent:", data);
+                return resolve({
+                    statusCode: 200
+                })
+            });
+        });
     } catch (err) {
         console.error("Error handling request:", err);
         return Promise.resolve({
